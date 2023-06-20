e42b3fc385298e3d46dbb558954db7a471c0f49b
HildaAdira <132376475+HildaAdira@users.noreply.github.com>
2023-06-20 18:37:51 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+//Hilaria Iwung
+//227064516036
+package gajiKaryawan;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Gaji Karyawan\t:");
+        System.out.println("================");
+    
+        Karyawan karyawan = new Karyawan();
+        karyawan.karyawan("8598979857", "Hilda", "perempuan");
+        karyawan.absenKerja();
+        karyawan.absenKerja();
+        karyawan.hitungGaji();
+        karyawan.displayInfo();
+
+        Dosen dosen = new Dosen();
+        dosen.karyawan("89797576874", "Raka", "Laki-Laki");
+        dosen.setGolongan("B");
+        dosen.setJumlahSks(4);
+        dosen.absenKerja();
+        dosen.absenKerja();
+        dosen.hitungGaji();
+        dosen.displayInfo();
+
+        Staf staf = new Staf();
+        staf.karyawan("9897845743", "Arumi", "Perempuan");
+        staf.absenKerja();
+        staf.hitungGaji();
+        staf.displayInfo();        
+       
+    }
+}
