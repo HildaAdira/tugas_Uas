74744b5a5d834e155e1c79cb674c9af57da7fc52
HildaAdira <132376475+HildaAdira@users.noreply.github.com>
2023-06-21 23:00:16 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,13 +7,13 @@ public class Main {
         System.out.println("================");
     
         Karyawan karyawan = new Karyawan();
-        karyawan.karyawan("8598979857", "Hilda", "perempuan");
+        karyawan.karyawan("LPW8979857", "Hilda", "perempuan");
         karyawan.absenKerja();
         karyawan.hitungGaji();
         karyawan.displayInfo();
 
         Dosen dosen = new Dosen();
-        dosen.karyawan("89797576874", "Raka", "Laki-Laki");
+        dosen.karyawan("SNR7576874", "Raka", "Laki-Laki");
         dosen.setGolongan("B");
         dosen.setJumlahSks(4);
         dosen.absenKerja();
@@ -21,7 +21,7 @@ public class Main {
         dosen.displayInfo();
 
         Staf staf = new Staf();
-        staf.karyawan("9897845743", "Arumi", "Perempuan");
+        staf.karyawan("SIP7845743", "Arumi", "Perempuan");
         staf.absenKerja();
         staf.hitungGaji();
         staf.displayInfo();        
